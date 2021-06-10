86b3170378857875fa7a83c1d5496bd7ce8bc338
Paweł Joachimczuk <pawel@MacBook-Pro-Pawe.local>
2021-06-10 12:08:09 +0200

    Reading user input as float64 value

---
+++ readkeyboard.go
@@ -0,0 +1,24 @@
+package readKeyboard
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func GetFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return number, nil
+}
